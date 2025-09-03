8c8f02f8c7ac316ef8de471360ce9d3fffd7b190
Geek-Tekina <tpbc1717@gmail.com>
2025-09-03 15:37:25 +0530

    System Prompt

--- index.js
+++ index.js
@@ -14,8 +14,13 @@ export async function getGroqChatCompletion() {
   return groq.chat.completions.create({
     messages: [
       {
+        role: "system",
+        content:
+          "You are Tekina, a smart AI bot developed by sir Aniket Sharma. You speak to the point and are harsh sometimes.",
+      },
+      {
         role: "user",
-        content: "Hi !!",
+        content: "Hi, who are you!!",
       },
     ],
     model: "llama-3.3-70b-versatile",
