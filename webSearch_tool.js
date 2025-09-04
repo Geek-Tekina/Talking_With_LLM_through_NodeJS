d2fd1f2bea285fb2fa55444611d47b1451687652
Geek-Tekina <tpbc1717@gmail.com>
2025-09-04 18:11:05 +0530

    Tool calling : webSearch() fnc as a tool

--- webSearch_tool.js
+++ webSearch_tool.js
@@ -43,14 +43,40 @@ async function main() {
   });
 
   // console.log(completions.choices[0].message);
+
   // Note : When a tool is called we do not get content in message, be get tool_calls. Meaning if tool_calls is not present , then we have final answer from the LLM.
-  console.log(JSON.stringify(completions.choices[0].message, null, 2));
+
+  // console.log(JSON.stringify(completions.choices[0].message, null, 2));
+
+  //LLM does not excecutes, it returns us a message, telling which tool we have to excecute programitaclly. And then we send the output of that tool to LLm, then at last LLM sends us the final output.
+
+  //we will check if any tool is called
+
+  const toolCalls = completions.choices[0].message.tool_calls;
+  if (!toolCalls) {
+    // this means we have our final answer from LLM
+    console.log(`Assitant : ${completions.choices[0].message.content}`);
+    return;
+  }
+
+  // if excecution comes here that means, any tool is been called. As tool_calls is an array, we have to loop over each, exceute the tool, and again send it's output to LLM as role = "tool".
+
+  for (const tool of toolCalls) {
+    // console.log(`tool : ${tool}`);
+    const functionName = tool.function.name;
+    const functionParams = tool.function.arguments;
+
+    if (functionName == "webSearch") {
+      const toolResponse = await webSearch(JSON.parse(functionParams));
+      console.log(`Tool Response : ${toolResponse}`);
+    }
+  }
 }
 
 async function webSearch() {
   // here we will be using Tavily to search for query on internet
 
-  return "It was launced in 2024, September.";
+  return "It was launched in 2024, September.";
 }
 
 main();
