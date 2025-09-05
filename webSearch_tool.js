7a79512366de5bc6f9a6a58e727e2ec3f51bcf36
Geek-Tekina <tpbc1717@gmail.com>
2025-09-05 19:42:32 +0530

    One while loop to automate the tool calling of LLM

--- webSearch_tool.js
+++ webSearch_tool.js
@@ -15,102 +15,109 @@ async function main() {
     },
     {
       role: "user",
-      content: "When was Iphone 16 launched ? ",
+      content: "When was the verdict of India - Pakistan war in 2025 ? ",
     },
   ];
-  const completions = await groq.chat.completions.create({
-    model: "llama-3.3-70b-versatile",
-    temperature: 0,
-    messages: messages,
-    tools: [
-      {
-        type: "function",
-        function: {
-          name: "webSearch",
-          description: "Search the latest and realtime data on the internet.",
-          parameters: {
-            type: "object",
-            properties: {
-              query: {
-                type: "string",
-                description: "The search query to perform search on.",
+
+  while (true) {
+    const completions = await groq.chat.completions.create({
+      model: "llama-3.3-70b-versatile",
+      temperature: 0,
+      messages: messages,
+      tools: [
+        {
+          type: "function",
+          function: {
+            name: "webSearch",
+            description: "Search the latest and realtime data on the internet.",
+            parameters: {
+              type: "object",
+              properties: {
+                query: {
+                  type: "string",
+                  description: "The search query to perform search on.",
+                },
               },
+              required: ["query"],
             },
-            required: ["query"],
           },
         },
-      },
-    ],
-    tool_choice: "auto",
-  });
+      ],
+      tool_choice: "auto",
+    });
 
-  // console.log(completions.choices[0].message);
+    // console.log(completions.choices[0].message);
 
-  // Note : When a tool is called we do not get content in message, be get tool_calls. Meaning if tool_calls is not present , then we have final answer from the LLM.
+    // Note : When a tool is called we do not get content in message, be get tool_calls. Meaning if tool_calls is not present , then we have final answer from the LLM.
 
-  // console.log(JSON.stringify(completions.choices[0].message, null, 2));
+    // console.log(JSON.stringify(completions.choices[0].message, null, 2));
 
-  //LLM does not excecutes, it returns us a message, telling which tool we have to excecute programitaclly. And then we send the output of that tool to LLm, then at last LLM sends us the final output.
+    //LLM does not excecutes, it returns us a message, telling which tool we have to excecute programitaclly. And then we send the output of that tool to LLm, then at last LLM sends us the final output.
 
-  // we will push the message from assistant to maintain history
-  messages.push(completions.choices[0].message);
+    // we will push the message from assistant to maintain history
+    messages.push(completions.choices[0].message);
 
-  //we will check if any tool is called
+    //we will check if any tool is called
 
-  const toolCalls = completions.choices[0].message.tool_calls;
-  if (!toolCalls) {
-    // this means we have our final answer from LLM
-    console.log(`Assitant : ${completions.choices[0].message.content}`);
-    return;
-  }
+    const toolCalls = completions.choices[0].message.tool_calls;
+    if (!toolCalls) {
+      // this means we have our final answer from LLM
+      console.log(`Assitant : ${completions.choices[0].message.content}`);
 
-  // if excecution comes here that means, any tool is been called. As tool_calls is an array, we have to loop over each, exceute the tool, and again send it's output to LLM as role = "tool".
+      // as there will be no tool call, we can break from this while loop
+      // return;
+      break;
+    }
 
-  for (const tool of toolCalls) {
-    // console.log(`tool : ${tool}`);
-    const functionName = tool.function.name;
-    const functionParams = tool.function.arguments;
+    // if excecution comes here that means, any tool is been called. As tool_calls is an array, we have to loop over each, exceute the tool, and again send it's output to LLM as role = "tool".
 
-    if (functionName == "webSearch") {
-      const toolResponse = await webSearch(JSON.parse(functionParams));
-      console.log(`Tool Response : ${toolResponse}`);
+    for (const tool of toolCalls) {
+      // console.log(`tool : ${tool}`);
+      const functionName = tool.function.name;
+      const functionParams = tool.function.arguments;
 
-      // we will also pass the toolresponse to messages so LLM has complete context about the convo
-      messages.push({
-        tool_call_id: tool.id,
-        role: "tool",
-        name: functionName,
-        content: toolResponse,
-      });
+      if (functionName == "webSearch") {
+        const toolResponse = await webSearch(JSON.parse(functionParams));
+        // console.log(`Tool Response : ${toolResponse}`);
+
+        // we will also pass the toolresponse to messages so LLM has complete context about the convo
+        messages.push({
+          tool_call_id: tool.id,
+          role: "tool",
+          name: functionName,
+          content: toolResponse,
+        });
+      }
     }
   }
-  const completions2 = await groq.chat.completions.create({
-    model: "llama-3.3-70b-versatile",
-    temperature: 0,
-    messages: messages,
-    tools: [
-      {
-        type: "function",
-        function: {
-          name: "webSearch",
-          description: "Search the latest and realtime data on the internet.",
-          parameters: {
-            type: "object",
-            properties: {
-              query: {
-                type: "string",
-                description: "The search query to perform search on.",
-              },
-            },
-            required: ["query"],
-          },
-        },
-      },
-    ],
-    tool_choice: "auto",
-  });
 
-  console.log(JSON.stringify(completions2.choices[0].message, null, 2));
+  // const completions2 = await groq.chat.completions.create({
+  //   model: "llama-3.3-70b-versatile",
+  //   temperature: 0,
+  //   messages: messages,
+  //   tools: [
+  //     {
+  //       type: "function",
+  //       function: {
+  //         name: "webSearch",
+  //         description: "Search the latest and realtime data on the internet.",
+  //         parameters: {
+  //           type: "object",
+  //           properties: {
+  //             query: {
+  //               type: "string",
+  //               description: "The search query to perform search on.",
+  //             },
+  //           },
+  //           required: ["query"],
+  //         },
+  //       },
+  //     },
+  //   ],
+  //   tool_choice: "auto",
+  // });
+
+  // console.log(JSON.stringify(completions2.choices[0].message, null, 2));
 }
 
 async function webSearch({ query }) {
